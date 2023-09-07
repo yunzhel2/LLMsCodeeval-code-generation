a188501dca480f2fdc29fafda565b3d9c588453d
hnulht <381874042@qq.com>
2023-09-07 23:45:11 +0800

    Latest commit

---
+++ scripts/eval_starcoder.py
@@ -0,0 +1,415 @@
+import re
+import json
+import torch
+import logging
+import argparse
+import warnings
+
+from pathlib import Path
+from datasets import load_dataset
+from transformers import AutoTokenizer, AutoModelForCausalLM
+from tenacity import retry, stop_after_attempt, wait_random_exponential
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--access_token', default='', type=str)
+    parser.add_argument('--checkpoint', default='bigcode/tiny_starcoder_py',
+                        choices=['bigcode/tiny_starcoder_py', 'bigcode/starcoder', 'bigcode/starcoderplus'], type=str)
+    parser.add_argument('--data_load_name', default='code_smell_data.jsonl',
+                        choices=['code_review_data.jsonl', 'code_smell_data.jsonl', 'code_test_data.jsonl'], type=str)
+    parser.add_argument('--result_save_name', default='code_smell_eval_starcoder.jsonl',
+                        choices=['code_review_eval_starcoder.jsonl', 'code_smell_eval_starcoder.jsonl',
+                                 'code_test_data_starcoder.jsonl'], type=str)
+    parser.add_argument('--log_file_name', default='code_smell_eval_starcoder.log',
+                        choices=['code_review_eval_starcoder.log', 'code_smell_eval_starcoder.log',
+                                 'code_test_data_starcoder.log'], type=str)
+    args = parser.parse_args()
+
+    return args
+
+
+@retry(wait=wait_random_exponential(min=1, max=60), stop=stop_after_attempt(6))
+def generate_text(prompt, temperature, max_new_tokens):
+    inputs = tokenizer(prompt, return_tensors='pt', add_special_tokens=False).to(device)
+    outputs = model.generate(
+        inputs['input_ids'],
+        max_new_tokens=max_new_tokens,
+        temperature=temperature,
+        do_sample=False,
+        pad_token_id=tokenizer.eos_token_id
+    ).to('cpu')
+    response = tokenizer.decode(outputs[0], skip_special_tokens=True)
+
+    return response.split('Assistant: ')[-1].strip()
+
+
+@retry(wait=wait_random_exponential(min=1, max=60), stop=stop_after_attempt(6))
+def count_message_tokens(content):
+    tokens = tokenizer(content)['input_ids']
+    num_tokens = len(tokens)
+
+    return num_tokens
+
+
+def add_smell(example):
+    code_uid = example['code_uid']
+    lang_cluster = example['lang_cluster']
+    smell_code = example['smell_code']
+    source_code = example['source_code']
+    user_message = f"""As an expert software developer with years of experience, please meticulously inspect the following smell code segment and categorize it into one of the following categories:
+- large class: A class contains too many fields/methods/lines of code.
+- data class: A class contains only fields and crude methods for accessing them.
+- blob: A class that concentrates too many responsibilities, controls and oversees too many different objects.
+- feature envy: A method accesses the data of another object more than its own data.
+- long method: A method contains too many lines of code.
+The detailed information are as follows:
+1. Programming language: {lang_cluster} 
+2. Smell code segment: 
+```
+{smell_code.strip()}
+```
+3. Source code containing code smells:
+```
+{source_code.strip()}
+```
+Respond only with one of the specified categories."""
+    prompt = technical_assistant + 'Human: ' + user_message.strip() + '\n\nAssistant: '
+
+    logging.info('code uid: ' + str(code_uid))
+
+    input_tokens = count_message_tokens(prompt)
+    logging.info('input tokens: ' + str(input_tokens))
+    if input_tokens > max_input_tokens:
+        logging.warning('Over input tokens limit: ' + str(code_uid))
+
+    try:
+        response = generate_text(
+            prompt=prompt,
+            temperature=temperature,
+            max_new_tokens=max_new_tokens
+        )
+        logging.info('response: ' + str(response))
+
+        if response is not None:
+            output_tokens = count_message_tokens(response)
+            logging.info('output tokens: ' + str(output_tokens))
+            if output_tokens > max_new_tokens:
+                logging.warning('Over output tokens limit ' + str(code_uid))
+
+            supported_smells = ['large class', 'long method', 'data class', 'blob', 'feature envy']
+            if all(supported_smell not in response.lower() for supported_smell in supported_smells):
+                logging.warning('Respond content is invalid value.')
+                smell = ''
+            else:
+                smell = ''
+                # Find the smell that first occurs in the response.
+                min_index = float('inf')
+                for supported_smell in supported_smells:
+                    first_index = response.lower().find(supported_smell)
+                    if first_index != -1 and first_index < min_index:
+                        min_index = first_index
+                        smell = supported_smell
+        else:
+            logging.warning('Respond content is none.')
+            smell = ''
+
+    except Exception as e:
+        logging.error('Failed to generate text: ' + e.__str__())
+        smell = ''
+
+    logging.info('smell: ' + str(smell))
+    example['smell'] = smell
+
+    return example
+
+
+def add_diff_tag(example):
+    code_uid = example['code_uid']
+    lang_cluster = example['lang_cluster']
+    old_code = example['old_code']
+    diff_hunk = example['diff_hunk']
+    user_message = f"""As an expert code reviewer with years of experience, please meticulously inspect the following code change and categorize its quality into one of the following categories:
+- 0: Good quality that no review comments required.
+- 1: Poor quality that requires review comments.
+The detailed information are as follows:
+1. Programming language: {lang_cluster} 
+2. Original version code: 
+```
+{old_code.strip()}
+```
+3. Code diff chunk:
+```
+{diff_hunk.strip()}
+```
+Respond only with the number: 0 or 1."""
+    prompt = technical_assistant + 'Human: ' + user_message.strip() + '\n\nAssistant: '
+
+    logging.info('code uid: ' + str(code_uid))
+
+    input_tokens = count_message_tokens(prompt)
+    logging.info('input tokens: ' + str(input_tokens))
+    if input_tokens > max_input_tokens:
+        logging.warning('Over input tokens limit: ' + str(code_uid))
+
+    try:
+        response = generate_text(
+            prompt=prompt,
+            temperature=temperature,
+            max_new_tokens=max_new_tokens
+        )
+        logging.info('response: ' + str(response))
+
+        if response is not None:
+            output_tokens = count_message_tokens(response)
+            logging.info('output tokens: ' + str(output_tokens))
+            if output_tokens > max_new_tokens:
+                logging.warning('Over output tokens limit ' + str(code_uid))
+
+            supported_diff_tags = ['0', '1']
+            if all(supported_diff_tag not in response for supported_diff_tag in supported_diff_tags):
+                logging.warning('Respond content is invalid value.')
+                diff_tag = 2
+            else:
+                diff_tag = 2
+                # Find the diff tag that first occurs in the response.
+                min_index = float('inf')
+                for supported_diff_tag in supported_diff_tags:
+                    first_index = response.find(supported_diff_tag)
+                    if first_index != -1 and first_index < min_index:
+                        min_index = first_index
+                        diff_tag = int(supported_diff_tag)
+        else:
+            logging.warning('Respond content is none.')
+            diff_tag = 2
+
+    except Exception as e:
+        logging.error('Failed to generate text: ' + e.__str__())
+        diff_tag = 2
+
+    logging.info('diff_tag: ' + str(diff_tag))
+    example['diff_tag'] = diff_tag
+
+    return example
+
+
+def add_review_comment(example):
+    code_uid = example['code_uid']
+    lang_cluster = example['lang_cluster']
+    old_code = example['old_code']
+    diff_hunk = example['diff_hunk']
+    user_message = f"""As an expert code reviewer with years of experience, please meticulously inspect the following code change and provide a concise review comment.
+The detailed information are as follows:
+1. Programming language: {lang_cluster} 
+2. Original version code: 
+```
+{old_code.strip()}
+```
+3. Code diff chunk:
+```
+{diff_hunk.strip()}
+```
+Respond only with a string that represents review comment."""
+    prompt = technical_assistant + 'Human: ' + user_message.strip() + '\n\nAssistant: '
+
+    logging.info('code uid: ' + str(code_uid))
+
+    input_tokens = count_message_tokens(prompt)
+    logging.info('input tokens: ' + str(input_tokens))
+    if input_tokens > max_input_tokens:
+        logging.warning('Over input tokens limit: ' + str(code_uid))
+
+    try:
+        response = generate_text(
+            prompt=prompt,
+            temperature=temperature,
+            max_new_tokens=max_new_tokens
+        )
+        logging.info('response: ' + str(response))
+
+        if response is not None:
+            output_tokens = count_message_tokens(response)
+            logging.info('output tokens: ' + str(output_tokens))
+            if output_tokens > max_new_tokens:
+                logging.warning('Over output tokens limit ' + str(code_uid))
+
+            review_comment = response
+        else:
+            logging.warning('Respond content is none.')
+            review_comment = ''
+
+    except Exception as e:
+        logging.error('Failed to generate text: ' + e.__str__())
+        review_comment = ''
+
+    logging.info('review_comment: ' + str(review_comment))
+    example['review_comment'] = review_comment
+
+    return example
+
+
+def add_hidden_unit_tests(example):
+    code_uid = example['code_uid']
+    prob_desc_description = example['prob_desc_description']
+    prob_desc_time_limit = example['prob_desc_time_limit']
+    prob_desc_memory_limit = example['prob_desc_memory_limit']
+    prob_desc_input_spec = example['prob_desc_input_spec']
+    prob_desc_output_spec = example['prob_desc_output_spec']
+    prob_desc_sample_inputs = example['prob_desc_sample_inputs']
+    prob_desc_sample_outputs = example['prob_desc_sample_outputs']
+    prob_desc_notes = example['prob_desc_notes']
+    source_code = example['source_code']
+    lang_cluster = example['lang_cluster']
+    num_hidden_unit_tests = example['num_hidden_unit_tests']
+    user_message = f"""As an expert code test developer with years of experience, please provide multiple test cases for a given problem along and its solution.
+The detailed information are as follows:
+1. Problem description: {prob_desc_description}
+2. Time limit: {prob_desc_time_limit}
+3. Memory limit: {prob_desc_memory_limit}
+4. Input specification: {prob_desc_input_spec}
+5. Output specification: {prob_desc_output_spec}
+6. Sample inputs: {prob_desc_sample_inputs}
+7. Sample outputs: {prob_desc_sample_outputs}
+8. Sample explanations: {prob_desc_notes}
+9. Programming language: {lang_cluster} 
+10. Solution source code: 
+```
+{source_code.strip()}
+```
+Craft {num_hidden_unit_tests} test cases with these criteria:
+1. Each test case contains a string for both input and output.
+2. The solution source code successfully processes the test case's input with no errors.
+3. The solution source code's outcome aligns with the test case's output.
+4. All test cases are simple and achieve optimal branch and line coverage.
+Respond only with a string in the following JSON format:
+[{{"input": input string, "output": output string}}]"""
+    prompt = technical_assistant + 'Human: ' + user_message.strip() + '\n\nAssistant: '
+
+    logging.info('code uid: ' + str(code_uid))
+
+    input_tokens = count_message_tokens(prompt)
+    logging.info('input tokens: ' + str(input_tokens))
+    if input_tokens > max_input_tokens:
+        logging.warning('Over input tokens limit: ' + str(code_uid))
+
+    try:
+        response = generate_text(
+            prompt=prompt,
+            temperature=temperature,
+            max_new_tokens=max_new_tokens
+        )
+        logging.info('response: ' + str(response))
+
+        if response is not None:
+            output_tokens = count_message_tokens(response)
+            logging.info('output tokens: ' + str(output_tokens))
+            if output_tokens > max_new_tokens:
+                logging.warning('Over output tokens limit ' + str(code_uid))
+
+            pattern = r'\[.*{.*?\}.*]'
+            matches = re.search(pattern, response, re.DOTALL)
+            if matches:
+                json_array_string = matches.group().replace("'", '"')
+                try:
+                    json_array = json.loads(json_array_string, strict=False)
+                    if isinstance(json_array, list):
+                        for json_item in json_array:
+                            if isinstance(json_item['input'], list):
+                                json_item['input'] = str(json_item['input'][0])
+                            if isinstance(json_item['output'], str):
+                                json_item['output'] = [json_item['output']]
+                        hidden_unit_tests = str(json_array)
+                    else:
+                        logging.warning('Respond content is not a list.')
+                        hidden_unit_tests = '[]'
+                except json.JSONDecodeError as e:
+                    logging.warning('Failed to load json:', e)
+                    hidden_unit_tests = '[]'
+            else:
+                logging.warning('JSON array object not found.')
+                hidden_unit_tests = '[]'
+
+        else:
+            logging.warning('Respond content is none.')
+            hidden_unit_tests = '[]'
+
+    except Exception as e:
+        logging.error('Failed to generate text: ' + e.__str__())
+        hidden_unit_tests = '[]'
+
+    logging.info('hidden_unit_tests: ' + str(hidden_unit_tests))
+    example['hidden_unit_tests'] = hidden_unit_tests
+
+    return example
+
+
+def main():
+    load_path = Path(__file__).parent.parent / Path('data') / Path(args.data_load_name)
+    save_path = Path(__file__).parent.parent / Path('results') / Path(args.result_save_name)
+
+    dataset = load_dataset('json', split='train', data_files=str(load_path))
+    dataset.cleanup_cache_files()  # for multiple evaluation
+    print(dataset)
+
+    if args.data_load_name == 'code_review_data.jsonl':
+        dataset = dataset.map(add_diff_tag)
+        dataset = dataset.map(add_review_comment)
+    elif args.data_load_name == 'code_smell_data.jsonl':
+        dataset = dataset.map(add_smell)
+    elif args.data_load_name == 'code_test_data.jsonl':
+        dataset = dataset.map(add_hidden_unit_tests)
+    print(dataset)
+
+    dataset.to_json(save_path, lines=True)
+
+
+if __name__ == '__main__':
+    warnings.filterwarnings('ignore')
+    args = parse_arguments()
+
+    log_file_path = Path(__file__).parent.parent / Path('logs') / Path(args.log_file_name)
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+    formatter = logging.Formatter(fmt='%(asctime)s - %(filename)s - %(levelname)s - %(message)s',
+                                  datefmt='%Y-%m-%d %H:%M:%S')
+    file_handler = logging.FileHandler(filename=log_file_path, mode='w', encoding='utf-8')
+    file_handler.setLevel(logging.INFO)
+    file_handler.setFormatter(formatter)
+    stream_handler = logging.StreamHandler()
+    stream_handler.setLevel(logging.INFO)
+    stream_handler.setFormatter(formatter)
+    logger.addHandler(file_handler)
+    logger.addHandler(stream_handler)
+
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    print('Device:', device)
+
+    # References: https://huggingface.co/blog/starcoder
+    # References: https://huggingface.co/datasets/bigcode/ta-prompt
+    # References: https://github.com/bigcode-project/starcoder/issues/101
+    tokenizer = AutoTokenizer.from_pretrained(
+        args.checkpoint,
+        use_fast=True,
+        trust_remote_code=True,
+        token=args.access_token
+    )
+    model = AutoModelForCausalLM.from_pretrained(
+        args.checkpoint,
+        torch_dtype=torch.float16,
+        # load_in_4bit=True,
+        low_cpu_mem_usage=True,
+        trust_remote_code=True,
+        device_map='auto',
+        token=args.access_token
+    )
+    print(f'Memory footprint: {model.get_memory_footprint() / 1e6:.2f} MB')
+    temperature = 0
+    max_input_tokens = tokenizer.model_max_length  # 1000000000000000019884624838656
+    # The maximum numbers of tokens to generate, ignoring the number of tokens in the prompt.
+    max_new_tokens = 1024
+    technical_assistant_path = Path(__file__).parent.parent / Path('data') / Path('TA_prompt_v1.txt')
+    with open(technical_assistant_path, mode='r', encoding='utf-8') as file:
+        technical_assistant = file.read() + '\n'
+
+    main()
+    # python scripts/eval_starcoder.py
